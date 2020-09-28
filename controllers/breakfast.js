df659c519013c06565eeb97af17730e817b51208
Venkat Kalyan <60262281+venkat-kalyan-au7@users.noreply.github.com>
2020-09-28 08:38:59 +0530

    Update breakfast.js

--- controllers/breakfast.js
+++ controllers/breakfast.js
@@ -17,7 +17,7 @@ exports.Breakfast=(req,res)=>{
     const {email}=req.body;
     const updateData={
         date:new Date().getDate()+"-"+(new Date().getMonth()+1)+'-'+new Date().getFullYear(),
-        time:(new Date().getHours()+6)+":"+new Date().getMinutes() +":"+ new Date().getSeconds(),
+        time:(new Date().getHours()+6)+":"+(new Date().getMinutes()-30) +":"+ (new Date().getSeconds()-5),
         cal:totalCal.toFixed(3)
     }
     if(totalCal>0){
